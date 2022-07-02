248ff8dbecfe6a9e9cdeb089c6e26fbf046e142c
t00256194 <108542965+t00256194@users.noreply.github.com>
2022-07-01 22:08:04 -0500

    Update and rename lab1_part2 to lab1_part2.m

--- lab1_part2.m
+++ lab1_part2.m
@@ -1,14 +1,14 @@
 clear all
 clc
 
-%%%%Equations
+%% Equations
 t = 0:pi/100:2*pi;
 y1 = exp(-0.5*t);
 y2 = exp(-0.5*t).*sin(2*t);
 y3 = exp(0.5*t);
 y4 = exp(0.5*t).*sin(2*t);
 
-%%%%Set1
+%% Set1
 figure
 subplot 121
 plot(t,y1,'b--'); hold on
@@ -16,7 +16,7 @@ plot(t,y2,'r-'); grid on
 title('Set 1 vs. Time'); legend('y1','y2')
 xlabel('Time (s)'); ylabel('Set 1')
 
-%%%%Set2
+%% Set2
 subplot 122
 plot(t,y3,'b--'); hold on
 plot(t,y4,'r-'); grid on
